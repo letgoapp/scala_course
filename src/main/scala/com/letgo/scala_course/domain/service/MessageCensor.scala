e9cade2dde04df6b54cdd4d4842bc9ea375f1fda
Diyan Krasimirov <diyan.krasimirov@letgo.com>
2017-02-10 16:25:08 +0100

    Solution

--- src/main/scala/com/letgo/scala_course/domain/service/MessageCensor.scala
+++ src/main/scala/com/letgo/scala_course/domain/service/MessageCensor.scala
@@ -4,7 +4,16 @@ import com.letgo.scala_course.domain.Message
 
 class MessageCensor(forbiddenKeywords: Set[String]) {
 
-  private val filterRule: Message => Message = ???
+  private val filterRule: Message => Message = { message =>
+    Message(
+      message.text.split(" ").filterNot(
+        word =>
+          isForbidden(word)
+      ).mkString(" ")
+    )
+  }
+
+  def isForbidden(word: String): Boolean = forbiddenKeywords.contains(word)
 
   def filterMessages(messages: Seq[Message]): Seq[Message] = messages.map(filterRule)
 }
